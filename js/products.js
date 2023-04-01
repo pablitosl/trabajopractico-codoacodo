e10451399e29703c79dc28602fb16d02207f3c35
Pablo Castro <pablocastrod7@gmail.com>
2023-04-01 17:13:06 -0300

    Agregando navbar a todas las vistas

--- js/products.js
+++ js/products.js
@@ -1,5 +1,26 @@
 //https://fakestoreapi.com/
+const API_URL = 'https://fakestoreapi.com/products/';
 
-fetch('https://fakestoreapi.com/products/')
-        .then(res=>res.json())
-        .then(json=>console.log(json))
+//fetch(API_URL)
+//        .then(res=>res.json())
+//        .then(json=>{
+//                const products = document.getElementById('products');
+//                json.forEach(product => {
+//                        const productElement = document.createElement('div');
+//                        productElement.classList.add('product');
+//                        productElement.innerHTML = `
+//                                <div class="product-image">
+//                                        <img src="${product.image}" alt="${product.title}">
+//                                </div>
+//                                <div class="product-info">
+//                                        <h3 class="product-title">${product.title}</h3>
+//                                        <p class="product-description">${product.description}</p>
+//                                        <p class="product-price">$${product.price}</p>
+//                                </div>
+//                        `;
+//                        products.appendChild(productElement);
+//                });
+//        })
+//
+
+
