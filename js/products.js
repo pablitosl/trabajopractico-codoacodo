b4bb0ab091362c4839b18dc4e3f54394cfabdcba
Pablo Castro <pablocastrod7@gmail.com>
2023-04-12 17:20:51 -0300

    Agregando la llamada a la api

--- js/products.js
+++ js/products.js
@@ -1,26 +1,25 @@
 //https://fakestoreapi.com/
 const API_URL = 'https://fakestoreapi.com/products/';
 
-//fetch(API_URL)
-//        .then(res=>res.json())
-//        .then(json=>{
-//                const products = document.getElementById('products');
-//                json.forEach(product => {
-//                        const productElement = document.createElement('div');
-//                        productElement.classList.add('product');
-//                        productElement.innerHTML = `
-//                                <div class="product-image">
-//                                        <img src="${product.image}" alt="${product.title}">
-//                                </div>
-//                                <div class="product-info">
-//                                        <h3 class="product-title">${product.title}</h3>
-//                                        <p class="product-description">${product.description}</p>
-//                                        <p class="product-price">$${product.price}</p>
-//                                </div>
-//                        `;
-//                        products.appendChild(productElement);
-//                });
-//        })
-//
+fetch(API_URL)
+        .then(res=>res.json())
+        .then(json=>{
+                const products = document.getElementById('products');
+                json.forEach(product => {
+                        const productElement = document.createElement('div');
+                        productElement.classList.add('product');
+                        productElement.innerHTML = `
+                                <div class="product-image">
+                                        <img src="${product.image}" alt="${product.title}">
+                                </div>
+                                <div class="product-info">
+                                        <h3 class="product-title">${product.title}</h3>
+                                        <p class="product-price">$${product.price}</p>
+                                </div>
+                        `;
+                        products.appendChild(productElement);
+                });
+        })
+
 
 
